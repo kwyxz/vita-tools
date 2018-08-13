4776bbfbb009c634ca11f1547a23b83450cf0d41
kwyxz <kwyxz@kwyxz.org>
2018-08-12 19:30:18 -0700

    Various fixes

--- vita_lpl.sh
+++ vita_lpl.sh
@@ -3,16 +3,19 @@
 MAMEBIN=/usr/local/games/mame/mame64
 VITA_IP=192.168.1.10
 VITA_PORT=1337
+HOMEBREWPATH=ux0:/homebrew
+ROMPATH=$HOMEBREWPATH/roms
+RETROPATH=ux0:/data/retroarch
 
 echo -n "Cleaning up local directory... "
 rm *.lpl
 echo done
 
 echo -n "Cleaning up remote directory on Vita... "
-lftp -c "open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /ux0:/data/retroarch ; mrm playlists/*" > /dev/null
+lftp -c "open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /$RETROPATH ; mrm playlists/*" > /dev/null
 echo done
 
-CONSOLELIST=$(lftp -c "open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /ux0:/homebrew/roms/ ; cls -1 " | tr -d $'\r' | sed -e 's/\/$//')
+CONSOLELIST=$(lftp -c "open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /$ROMPATH ; cls -1 " | tr -d $'\r' | sed -e 's/\/$//')
 
 _mame ()
 {
@@ -27,28 +30,33 @@ _mame ()
 
 _getname ()
 {
-  GAME=$(echo "$1" | tr '_' ' ' | tr -d '[!]')
-  FULLNAME=$(basename "$GAME" "$2")
+  if $(echo "$1" | grep -q "$2")
+  then
+    GAME=$(echo "$1" | tr '_' ' ' | tr -d '[!]')
+    if $(echo "$GAME" | grep -q \.zip)
+    then
+      FULLNAME=$(basename "$GAME" "$2.zip")
+    else
+      FULLNAME=$(basename "$GAME" "$2")
+    fi
+  else
+    echo "$1" has an unrecognized extension, skipping
+    SKIP=1
+  fi
 }
 
 for CONSOLE in $CONSOLELIST
 do
   echo Generating playlist for : $CONSOLE
 
-  COMMAND="open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /ux0:/homebrew/roms/$CONSOLE/ ; cls -1 "
+  COMMAND="open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /$ROMPATH/$CONSOLE/ ; cls -1 "
   GAMELIST=$(lftp -c "$COMMAND" | tr ' ' '_' | tr -d $'\r')
   FULLNAME=""
 
   for GAMENAME in $GAMELIST
   do
+  SKIP=0
     case $CONSOLE in
-      32x)
-        EXTENSION=".32x"
-        PLAYLIST="Sega - 32X.lpl"
-        LIBRETRO="app0:/picodrive_libretro.self"
-        LIBNAME="Picodrive"
-        _getname "$GAMENAME" "$EXTENSION"
-        ;;
       fba|neogeo|cps[1-2])
         PLAYLIST="FB Alpha - Arcade Games.lpl"
         LIBRETRO="app0:/fbalpha_libretro.self"
@@ -84,7 +92,7 @@ do
         _getname "$GAMENAME" "$EXTENSION"
         ;;
       md)
-        EXTENSION=".gen"
+        EXTENSION=".md"
         PLAYLIST="Sega - Mega Drive - Genesis.lpl"
         LIBRETRO="app0:/genesis_plus_gx_libretro.self"
         LIBNAME="Genesis Plus GX"
@@ -138,11 +146,25 @@ do
         LIBNAME="Mednafen PCE Fast"
         _getname "$GAMENAME" "$EXTENSION"
         ;;
+      pcecd)
+        EXTENSION=".cue"
+        PLAYLIST="NEC - PC Engine CD - TurboGrafx-CD.lpl"
+        LIBRETRO="app0:/mednafen_pce_fast_libretro.self"
+        LIBNAME="Mednafen PCE Fast"
+        _getname "$GAMENAME" "$EXTENSION"
+        ;;
       ps1)
         EXTENSION=".cue"
         PLAYLIST="Sony - PlayStation.lpl"
         LIBRETRO="app0:/pcsx_rearmed_libretro.self"
         LIBNAME="PCSX ReArmed"
+        _getname "$GAMENAME" "$EXTENSION"
+        ;;
+      sg1000)
+        EXTENSION=".sg"
+        PLAYLIST="Sega - SG-1000.lpl"
+        LIBRETRO="app0:/genesis_plus_gx_libretro.self"
+        LIBNAME="Genesis Plus GX"
         _getname "$GAMENAME" "$EXTENSION"
         ;;
       sms)
@@ -181,15 +203,18 @@ do
 
   if [[ ! -z $PLAYLIST ]]
   then
-    FULLPATH=$(echo ux0:/homebrew/roms/$CONSOLE/$GAMENAME | tr '_' ' ')
-    CRC32="00000000"
-    echo "$FULLPATH" >> "$PLAYLIST"
-    echo "$FULLNAME" >> "$PLAYLIST"
-    echo "$LIBRETRO" >> "$PLAYLIST"
-    echo "$LIBNAME" >> "$PLAYLIST"
-    echo "$CRC32|crc" >> "$PLAYLIST"
-    echo "$PLAYLIST" >> "$PLAYLIST"
-    echo -n .
+    if [[ $SKIP -eq 0 ]]
+    then
+      FULLPATH=$(echo $ROMPATH/$CONSOLE/$GAMENAME | tr '_' ' ')
+      CRC32="00000000"
+      echo "$FULLPATH" >> "$PLAYLIST"
+      echo "$FULLNAME" >> "$PLAYLIST"
+      echo "$LIBRETRO" >> "$PLAYLIST"
+      echo "$LIBNAME" >> "$PLAYLIST"
+      echo "$CRC32|crc" >> "$PLAYLIST"
+      echo "$PLAYLIST" >> "$PLAYLIST"
+      echo -n .
+    fi
   fi
 
   done
@@ -197,7 +222,7 @@ echo
 done
 
 echo -n "Uploading playlists to Vita... "
-lftp -c "open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /ux0:/data/retroarch/playlists ; mput *.lpl" > /dev/null
+lftp -c "open -u anonymous,blah $VITA_IP:$VITA_PORT ; cd /$RETROPATH/playlists ; mput *.lpl" > /dev/null
 echo "done"
 
 exit 0
